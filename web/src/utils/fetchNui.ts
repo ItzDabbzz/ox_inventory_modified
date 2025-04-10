fc6d9ca3f9db561ed05e782cbc28b9ef85c4a54f
ItzDabbzz <itzdabbzz@gmail.com>
2025-04-10 03:24:13 -0700

    update booty fetchNui message

--- web/src/utils/fetchNui.ts
+++ web/src/utils/fetchNui.ts
@@ -14,7 +14,11 @@ import { isEnvBrowser } from './misc';
 const resourceName = (window as any).GetParentResourceName ? (window as any).GetParentResourceName() : 'ox_inventory';
 
 export async function fetchNui<T>(eventName: string, data?: unknown): Promise<T> {
-  if (isEnvBrowser()) return undefined as any; // HACK FOR BORING ERRORS IN DEV
+  /**
+   * This is a workaround for the fact that the NUI callbacks are not available in the browser environment.
+   * This is a temporary solution until we can find a better way to handle this.
+   */
+  if (isEnvBrowser()) return undefined as any;
 
   try {
     const resp = await fetch(`https://${resourceName}/${eventName}`, {
